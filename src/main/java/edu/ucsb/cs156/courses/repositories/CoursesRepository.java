9bd5b2ebde3a97072dbef33b4e64dcfd7bbdbf12
Zhengyan Yu <zhengyan@umail.ucsb.edu>
2022-06-02 14:24:52 -0700

    solve merge comfilct

--- src/main/java/edu/ucsb/cs156/courses/repositories/CoursesRepository.java
+++ src/main/java/edu/ucsb/cs156/courses/repositories/CoursesRepository.java
@@ -4,10 +4,18 @@ import edu.ucsb.cs156.courses.entities.Courses;
 import edu.ucsb.cs156.courses.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
 
 @Repository
 public interface CoursesRepository extends CrudRepository<Courses, Long> {
-    Iterable<Courses> findAllByPsId(Long psId);
+  Optional<Courses> findByPsId(Long psId);
+  Optional<Courses> findById(Long id);
+  Optional<Courses> findByIdAndUser(long id, User user);
+
+  Iterable<Courses> findAllByPsId(Long psId);
+  Iterable<Courses> findAllByPsIdAndUser(Long psId, User user);
+  Iterable<Courses> findAllByUserId(Long user_id);
 }
+
